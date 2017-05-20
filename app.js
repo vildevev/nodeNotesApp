0cc5350e33e6cb0e9ac51d1122e9e47355d55ed5
“Vilde <vilde@vevatne.no>
2017-05-20 13:29:59 -0700

    Gets username with os-module

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 console.log("Starting app");
 
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFile('greetings.txt', 'Hello World', function (err) {
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt',`Hello ${user.username}!` , function (err) {
   if (err) {
     console.log('Unable to write file');
   }

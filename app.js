9ca6994bdd5cd3c2d83db6ae615d81404d7d04c5
“Vilde <vilde@vevatne.no>
2017-05-20 13:24:18 -0700

    Adds fs and appendfile function

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Starting app"); 
+console.log("Starting app");
+
+const fs = require('fs');
+
+fs.appendFile('greetings.txt', 'Hello World', function (err) {
+  if (err) {
+    console.log('Unable to write file');
+  }
+});

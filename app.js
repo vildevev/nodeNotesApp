3560a86f1cee88ab20e8167ec6b325d2d1ee40db
“Vilde <vilde@vevatne.no>
2017-05-21 11:18:36 -0700

    Adds lodash

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ console.log("Starting app.js");
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
+const _ = require('lodash');
 
+console.log(_.isString(true));
+console.log(_.isString("hello")); 
  var res = notes.addNumbers(2, 4);
- console.log(res); 
+ console.log(res);
 
 // var user = os.userInfo();
 

3ec1677370166921569ca050a87030a724af905e
“Vilde <vilde@vevatne.no>
2017-05-21 10:51:14 -0700

    Requires notes.js file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-console.log("Starting app");
+console.log("Starting app.js");
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes'); 
 
 var user = os.userInfo();
 
